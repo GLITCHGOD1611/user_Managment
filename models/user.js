b025fd7e10de0ab6194d968319bfbdb99327afdc
omkar deshmane <omkardeshmane832@gmail.com>
2026-01-29 18:55:36 +0530

    updated mongo connect

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 const userSchema = mongoose.Schema({
 
